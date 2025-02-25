2a78ea4713ff66c69ae940921367b0db440f5634
CreepTV <marco@rey-leimen.de>
2025-02-25 22:52:38 +0100

    Initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -39,4 +39,10 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             document.getElementById('navbar-container').innerHTML = data;
         });
+
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-container').innerHTML = data;
+        });
 });
